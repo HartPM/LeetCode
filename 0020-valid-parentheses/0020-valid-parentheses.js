af73696f6d2cb5d064182f28cedfa891badd65cd
Patrick Hart <101307197+HartPM@users.noreply.github.com>
2022-10-10 15:08:46 -0400

    Time: 66 ms (95.64%), Space: 42.6 MB (54.61%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  if (s.length % 2 !== 0) return false;
+
+  let a = [];
+  for (let i = 0; i < s.length; i++) {
+    switch(s[i]){
+      case '(':
+          a.push(')')
+          break;
+      case '[':
+        a.push(']')
+        break;
+      case '{':
+        a.push('}')
+        break;
+      case ')':
+        if (s[i] === ')' && a[a.length - 1] === ')'){
+          a.pop()
+          break;
+        } else return false;
+      case ']':
+        if (s[i] === ']' && a[a.length - 1] === ']'){
+          a.pop()
+          break;
+        } else return false;
+      case '}':
+        if (s[i] === '}' && a[a.length - 1] === '}'){
+          a.pop()
+          break;
+        } else return false;
+    }
+  }
+  if (a.length === 0) return true;
+  return false;
+};
