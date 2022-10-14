32c7fdfef117e5287a35e155124c57f937f17384
Patrick Hart <101307197+HartPM@users.noreply.github.com>
2022-10-14 14:15:26 -0400

    Time: 1382 ms (5.01%), Space: 44.4 MB (61.01%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -17,12 +17,9 @@ var missingNumber = function(nums) {
         if(noSwaps) break;
     }
     if(nums[0] !== 0) return 0;
-    for(let i=0; i<nums.length-1; i++){
+    for(let i=0; i<nums.length; i++){
       if(nums[i+1] !== (nums[i] + 1)){
         return nums[i] + 1;
       }        
-    }
-    if(nums[nums.length-1] !== nums.length) {
-      return nums.length;
     }
 };
